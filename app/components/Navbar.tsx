16b172805e9e047e6e6017a1063a758e558d1195
mig-gonz <mig256@outlook.com>
2023-08-26 17:36:38 -0700

    added Link to all users page for an example of the get all users data method from usersAPI

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,7 +4,7 @@ import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 
 const navigation = [
-  { name: "UsersAPI", href: "#", current: false },
+  { name: "UsersAPI", href: "/", current: false },
   { name: "Developer", href: "#", current: false },
   { name: "About", href: "#", current: false },
   { name: "API", href: "#", current: false },
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,23 @@
-import Image from "next/image";
-import Navbar from "./components/Navbar";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
+      <div className="mt-5 bg-base-200">
+        <div className="ml-5">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-bold">Mock Data</h1>
+            <div className="flex flex-col items-center">
+              <h2 className="mt-10">
+                View an example of get all users for your application:
+              </h2>
+              <Link href={"/users"}>
+                <button className="btn btn-wide btn-accent mt-2">/users</button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
